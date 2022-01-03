89fffb8c6a17b21f39255d888ecb2a7d195b789a
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-02 22:20:58 -0300

    cree una nueva rama

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
-var dateToday = new Date();
-let addZeroes = (num) => {
-    return num < 10 ? `0${num}`: num;
-}
-
-let hr = addZeroes(dateToday.getHours());
-let min = addZeroes(dateToday.getMinutes());
-let sec = addZeroes(dateToday.getSeconds());
-
-//console.log(hr, min, sec); 
-
-document.getElementById("time").textContent = `${hr}:${min}:${sec}`
+let dateToday = new Date();
